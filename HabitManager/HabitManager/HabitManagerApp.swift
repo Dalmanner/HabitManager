13049db3fc1d5cf2f5dc12ce0de372191837e732
Dalmanner <145015597+Dalmanner@users.noreply.github.com>
2024-06-17 06:19:50 +0200

    first commit

---
+++ HabitManager/HabitManager/HabitManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  HabitManagerApp.swift
+//  HabitManager
+//
+//  Created by Mac on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct HabitManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
